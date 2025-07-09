bca3c42229e2770c10d98805b7cc97e5bc1ee24b
phatmvp <phat0972430986@gmail.com>
2025-07-09 17:19:14 +0700

    update thu vien

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,13 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+
+    // Retrofit + Gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // ✅ OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
 }
