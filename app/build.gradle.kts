6351ee6700bbcd4164a8d5a0cb76dbcc087312aa
tqhuy14 <blackoccen@gmail.com>
2025-07-15 10:26:01 +0700

    ParkingOwnerDOne

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,13 +44,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-//    implementation(libs.navigation.fragment)
-//    implementation(libs.navigation.ui)
-//    implementation(libs.play.services.location)
-//    implementation(libs.play.services.maps)
+
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-
+    implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
 
     testImplementation(libs.junit)
